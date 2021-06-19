9856dca26871b3f59e3ca2189deb8ac02166635f
svmiller <steven.v.miller@gmail.com>
2021-06-19 08:31:52 -0400

    This will have to do, I think. There's really not a good way to test template rendering without also hiding some of features that are possible.

--- tests/testthat/test-exists.R
+++ tests/testthat/test-exists.R
@@ -1,10 +1,20 @@
 context("Does template exist")
 
-templates_all <- list.files("inst/rmarkdown/templates")
+#templates_all <- list.files("inst/rmarkdown/templates")
+templates_all <- list.files(system.file("rmarkdown", "templates", package="stevetemplates"))
 
-
-for (i in templates_all) {
+for (i in templates_all[c(1:5,7:10)]) {
 
   expect_true(file.exists(system.file("rmarkdown", "templates", i, "resources", "template.tex", package = "stevetemplates")))
 }
 
+
+for (i in templates_all[c(6)]) {
+
+  expect_true(file.exists(system.file("rmarkdown", "templates", i, "resources", "template.html", package = "stevetemplates")))
+}
+
+for (i in templates_all[c(11)]) {
+
+  expect_true(file.exists(system.file("rmarkdown", "templates", i, "resources", "template.docx", package = "stevetemplates")))
+}
