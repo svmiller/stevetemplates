7cc73c4fca3bf37e7069b680d5e02c5b8eff4379
svmiller <steven.v.miller@gmail.com>
2024-01-26 16:48:49 +0100

    dsfs

--- cran-comments.md
+++ cran-comments.md
@@ -9,3 +9,7 @@ R CMD check done via `devtools::check()`, resulting in 0 errors, 0 warnings, 0 n
 ## Downstream dependencies
 
 This is a simple template package with no downstream dependencies to note.
+
+## Feedback to CRAN upon initial rejection
+
+That was me clumsily adding new templates while my tests were hard-connected to elements in a vector, by number. That's my bad.
--- tests/testthat/test-exists.R
+++ tests/testthat/test-exists.R
@@ -3,18 +3,18 @@ context("Does template exist")
 #templates_all <- list.files("inst/rmarkdown/templates")
 templates_all <- list.files(system.file("rmarkdown", "templates", package="stevetemplates"))
 
-for (i in templates_all[c(1:6,8:11)]) {
+for (i in templates_all[which(templates_all != "html_article" & templates_all != "word")]) {
 
   expect_true(file.exists(system.file("rmarkdown", "templates", i, "resources", "template.tex", package = "stevetemplates")))
 }
 
 
-for (i in templates_all[c(7)]) {
+for (i in templates_all[which(templates_all == "html_article")]) {
 
   expect_true(file.exists(system.file("rmarkdown", "templates", i, "resources", "template.html", package = "stevetemplates")))
 }
 
-for (i in templates_all[c(12)]) {
+for (i in templates_all[which(templates_all == "word")]) {
 
   expect_true(file.exists(system.file("rmarkdown", "templates", i, "resources", "template.docx", package = "stevetemplates")))
 }
